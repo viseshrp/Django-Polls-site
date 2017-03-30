05049719ca9516a5dd066e562bebac584d3fc078
Visesh <viseshrprasad@gmail.com>
2017-03-30 14:29:04 -0400

    Change fields order in admin model

--- pollsite/polls/admin.py
+++ pollsite/polls/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
 from .models import Question
 
-# Register your models here.
-admin.site.register(Question)
+# Register your models here to make it show up in the admin interface.
+
+#admin.site.register(Question)
+
+class QuestionAdmin(admin.ModelAdmin):
+    fields = ['pub_date', 'question_text']
+
+admin.site.register(Question, QuestionAdmin)
