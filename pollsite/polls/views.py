3255898b46f421991e3fd9008cc456146103b245
Visesh <viseshrprasad@gmail.com>
2017-03-30 11:49:34 -0400

    Add generic views

--- pollsite/polls/views.py
+++ pollsite/polls/views.py
@@ -1,15 +1,50 @@
-from django.http import Http404
 from django.http import HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
 from django.urls import reverse
+from django.views import generic
 
 from .models import Question, Choice
 
 
 # Create your views here.
 
+# use generic views
 
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        """Get last 5 published questions"""
+        return Question.objects.order_by('-pub_date')[:5]
+
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
+
+
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        # when there's no choice selected, re display the vote form
+        return render(request, 'polls/detail.html', {'question': question, 'error_message': "No choice, idiot!", })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+
+
+# return HttpResponse("Voting on question %s." % question_id)
+
+'''
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
     #    template = loader.get_template('polls/index.html')
@@ -29,18 +64,4 @@ def results(request, question_id):
     return render(request, 'polls/results.html', {'question': question})
     # response = "Results of question %s."
     # return HttpResponse(response % question_id)
-
-
-def vote(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    try:
-        selected_choice = question.choice_set.get(pk=request.POST['choice'])
-    except (KeyError, Choice.DoesNotExist):
-        # when there's no choice selected, re display the vote form
-        return render(request, 'polls/detail.html', {'question': question, 'error_message': "No choice, idiot!", })
-    else:
-        selected_choice.votes += 1
-        selected_choice.save()
-        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
-
-# return HttpResponse("Voting on question %s." % question_id)
+'''
