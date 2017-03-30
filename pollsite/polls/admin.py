698a9affc58dc8b1779796e7bde8ffae1a8c5b87
Visesh <viseshrprasad@gmail.com>
2017-03-30 14:42:03 -0400

    Admin : display choices while adding question

--- pollsite/polls/admin.py
+++ pollsite/polls/admin.py
@@ -1,11 +1,25 @@
 from django.contrib import admin
-from .models import Question
+from .models import Question, Choice
+
 
 # Register your models here to make it show up in the admin interface.
 
-#admin.site.register(Question)
+# admin.site.register(Question)
+
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 3
+
 
 class QuestionAdmin(admin.ModelAdmin):
-    fields = ['pub_date', 'question_text']
+    fieldsets = [
+        (None, {'fields': ['question_text']}),
+        ('Date Info', {'fields': ['pub_date']}),
+    ]
+
+    inlines = [ChoiceInline]
+
 
 admin.site.register(Question, QuestionAdmin)
+
+# admin.site.register(Choice)
