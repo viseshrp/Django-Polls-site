6c101730cef626ef2ba2a494053a278c6ad525ae
Visesh <viseshrprasad@gmail.com>
2017-03-30 13:46:09 -0400

    add test and fix bug

--- pollsite/polls/tests.py
+++ pollsite/polls/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
 
+import datetime
+
+from django.utils import timezone
+
+from .models import Question
+
 # Create your tests here.
+
+class QuestionMethodTests(TestCase):
+
+    def test_was_published_recently(self): #name has to begin with test
+        time = timezone.now() + datetime.timedelta(days=30)
+        future_question = Question(pub_date=time)
+        self.assertIs(future_question.was_published_recently(), False)
