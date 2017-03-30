83bb708a62a5e6282362040e71ded0a7c8f132b8
Visesh <viseshrprasad@gmail.com>
2017-03-30 10:31:48 -0400

    Views : add & update results, details, voting

--- pollsite/polls/views.py
+++ pollsite/polls/views.py
@@ -1,8 +1,10 @@
 from django.http import Http404
+from django.http import HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+from django.urls import reverse
 
-from .models import Question
+from .models import Question, Choice
 
 
 # Create your views here.
@@ -23,9 +25,22 @@ def detail(request, question_id):
 
 
 def results(request, question_id):
-    response = "Results of question %s."
-    return HttpResponse(response % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
+    # response = "Results of question %s."
+    # return HttpResponse(response % question_id)
 
 
 def vote(request, question_id):
-    return HttpResponse("Voting on question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        # when there's no choice selected, re display the vote form
+        return render(request, 'polls/detail.html', {'question': question, 'error_message': "No choice, idiot!", })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+
+# return HttpResponse("Voting on question %s." % question_id)
